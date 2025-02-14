4c22f05537ed23f0ad3bb69bce6888394a6b4bbe
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-02-14 11:46:27 +0100

    test

--- main/src/main/Logic.java
+++ main/src/main/Logic.java
@@ -2,7 +2,12 @@ package main;
 
 public class Logic {
     public static void main(String[] args) {
-        
+        System.out.println("Hello World!");
+        int i = 5;
+        while (i >= 0){
+            System.out.println(i);
+            i--;
+        }
     }
 }
 
