d600d18a3ddf405f538fd413477a2e6e19e10673
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-02-15 05:38:00 +0100

    Test

--- main/src/main/Logic.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-public class Logic {
-    public static void main(String[] args) {
-    }
-}
---
+++ main/src/main/ModuleBasics_Medium.java
@@ -0,0 +1,17 @@
+package main;
+import java.util.Scanner;
+
+public class ModuleBasics_Medium {
+    public static void main(String[] args) {
+        //M1: Sterrenpiramide, opnieuw. Oprechte excuses
+        //
+        //Maak een variabele hoogte, hiervoor gebruik je een klein geheel getal. We gaan aan de hand van hoogte een sterrenpiramide bouwen.
+        // Per laag van de piramide komt er een ster bij. Dus bijvoorbeeld als hoogte gelijk is aan 4 zal de afgedrukte piramide er zo uit zien:
+        //
+        //    *
+        //    **
+        //    ***
+        //    ****
+        short hoogte = 4;
+    }
+}
